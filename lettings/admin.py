6b098cb72116bcfabb95d731aafa8e2db923ef57
zolatso <tomsaunders1@gmail.com>
2025-09-16 08:53:06 +0200

    refactoring templates

--- lettings/admin.py
+++ lettings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Letting
+from .models import Address
+
+admin.site.register(Letting)
+admin.site.register(Address)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+
+def index(request):
+    profiles_list = Profile.objects.all()
+    context = {"profiles_list": profiles_list}
+    return render(request, "profiles/index.html", context)
+
+
+def profile(request, username):
+    profile = Profile.objects.get(user__username=username)
+    context = {"profile": profile}
+
+    return render(request, "profiles/profile.html", context)
